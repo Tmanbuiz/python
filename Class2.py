3ca2d4a5c70b1c8e1a33282a46f766485dec56ea
tmanbuiz <atoyejet@gmail.com>
2023-12-21 07:00:38 -0400

    this is my first time committing on git2

---
+++ Class2.py
@@ -0,0 +1,34 @@
+# x = 34
+# print(x)
+# print(type(x))
+
+# x = 'hello world'
+# print(len(x))
+# print(x[1])
+# print(x[2:7])
+# print(x[:8])
+# print(x[2:])
+# print(x[-3])
+# print(x[-9:-3])
+# print(x[:-6])
+# print(x.upper())
+# print(x.count("p"))
+# print(x.rstrip())
+# print(x.lstrip())
+
+# # concatenation
+# x = "   hello    "
+# name = "parach"
+# print(x+name)
+# print(x.rstrip()+name)
+
+# #MyName = input(('Enter your name:'))
+# #greet = 'Hello, '+MyName
+# #print(greet)
+number = 11 % 12
+print(number)
+
+x = "z"
+x *= 3
+
+print(x)
